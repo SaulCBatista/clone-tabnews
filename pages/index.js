c5c49cc5625b16437f32d1bf475d94577a98fe4a
Saul Contêlo Batista <129624512+SaulCBatista@users.noreply.github.com>
2024-12-09 21:05:35 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Brenda, eu amo você! Se você me ama, dá uma risadinha! 😘</h1>
+  return <h1>Brenda, eu amo você! Se você me ama, dá uma risadinha! 😘</h1>;
+}
+
+function teste() {
+  console.log("indetação errada");
 }
 
 export default Home;
