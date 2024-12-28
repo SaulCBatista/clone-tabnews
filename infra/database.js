af0a02cdfa5180582f5c565e8e44959b2bf8a92a
Saul Contêlo Batista <129624512+SaulCBatista@users.noreply.github.com>
2024-12-28 14:58:37 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,13 +7,21 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
+  });
+
+  console.log("Credenciais do Postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
   });
 
   try {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
-  } catch {
+  } catch (error) {
     console.error(error);
     throw error;
   } finally {
